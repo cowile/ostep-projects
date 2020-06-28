13f39063aa0be5ccd2c08e4d537796122b98718a
Brian Li <brian.li@rubrik.com>
2020-06-27 20:30:39 -0700

    Add wgrep

---
+++ initial-utilities/wgrep/wgrep.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void grep(char *term, FILE *fp) {
+	char *line = NULL;
+	size_t linecap;
+	while (getline(&line, &linecap, fp) > 0) {
+		if (strstr(line, term) != NULL) {
+			std::cout << line;
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	// Check argument counts
+	if (argc <= 1) {
+		std::cout << "wgrep: searchterm [file ...]\n";
+		return 1;
+	}
+
+	char *term = argv[1];
+
+	// Grep from stdin
+	if (argc == 2) {
+		grep(term, stdin);
+		return 0;
+	}
+
+	// Grep in each file provided
+	for(int i = 2; i < argc; i++) {
+		FILE *fp = fopen(argv[i], "r");
+		if (fp == NULL) {
+			std::cout << "wgrep: cannot open file\n";
+			return 1;
+		}
+		grep(term, fp);
+		fclose(fp);
+	}
+	return 0;
+}
+
