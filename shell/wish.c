06f75e0153c0952e880eb3dd7145ce5b4fc5ac9c
Colton Lewis <colton.w.lewis@protonmail.com>
2020-09-19 13:25:49 -0500

    Implement absolute path conversion.

--- shell/wish.c
+++ shell/wish.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,9 +8,8 @@ const char *ERROR = "An error has occurred\n";
 const char REDIRECT_OUTPUT = '>';
 const char PARALLEL = '&';
 
-char *SEARCH_PATH[] = {
-	"/bin"
-};
+char **SEARCH_PATH;
+size_t SEARHCH_PATH_LEN;
 
 void shell_exit(int exit_code)
 {
@@ -18,12 +18,39 @@ void shell_exit(int exit_code)
 
 int shell_chdir(char *path)
 {
-	return chdir(path);
+	if(chdir(path) == -1)
+	{
+		fprintf(stdout, ERROR);
+	}
 }
 
-int shell_path(char paths)
+int shell_path(char **paths, size_t n)
 {
+	for(size_t i = 0; i < n; i++)
+	{
+		free(SEARCH_PATH[i]);
+	}
+	free(SEARCH_PATH);
+
+	SEARCH_PATH = paths;
+	SEARCH_PATH_LEN = n;
+
 	return 0;
+}
+
+char *absolute_path(char *name)
+{
+	char path[1024];
+	for(size_t i = 0; i < SEARCH_PATH_LEN; i++)
+	{
+		snprintf(path, 1024, "%s/%s", SEARCH_PATH[i], name);
+		if(access(path, X_OK) == 0)
+		{
+			return strdup(path);
+		}
+	}
+	
+	return NULL:
 }
 
 int main(int argc, char **argv)
