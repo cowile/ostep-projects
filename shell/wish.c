c5059d013f1c577d3dbf558d48f5a6455491606e
Colton Lewis <colton.w.lewis@protonmail.com>
2020-09-19 13:00:58 -0500

    Write skeleton of built-in commands.

--- shell/wish.c
+++ shell/wish.c
@@ -1,1 +1,32 @@
-//Add your code here
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+const char *PROMPT = "wish> ";
+const char *ERROR = "An error has occurred\n";
+const char REDIRECT_OUTPUT = '>';
+const char PARALLEL = '&';
+
+char *SEARCH_PATH[] = {
+	"/bin"
+};
+
+void shell_exit(int exit_code)
+{
+	exit(exit_code);
+}
+
+int shell_chdir(char *path)
+{
+	return chdir(path);
+}
+
+int shell_path(char paths)
+{
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	return EXIT_SUCCESS;
+}
