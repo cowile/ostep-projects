d672999db66cf97f06e8d90b0a98ba3f9d092639
Colton Lewis <colton.w.lewis@protonmail.com>
2020-09-19 23:05:04 -0500

    Make a second attempt.

--- shell/wish.c
+++ shell/wish.c
@@ -1,100 +1,47 @@
 #include <errno.h>
+#include <error.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+const char *CMD_SEP = "&\n";
+const char *ARG_SEP = " \f\r\t\v";
 const char *PROMPT = "wish> ";
 const char *ERROR = "An error has occurred\n";
 
-char **SEARCH_PATHS;
-size_t SEARCH_PATHS_LEN;
+char **SEARCH_PATHS = NULL;
+size_t SEARCH_PATHS_LEN = 0;
 
-void shell_exit(int exit_code)
+void update_search_paths(char **paths, size_t paths_len)
 {
-	exit(exit_code);
 }
 
-void shell_chdir(char *path)
+char *absolute_path(char *prog_name)
 {
-	if(chdir(path) == -1)
-	{
-		fprintf(stderr, ERROR);
-	}
-}
-
-void shell_path(char **paths, size_t n)
-{
-	for(size_t i = 0; i < SEARCH_PATHS_LEN; i++)
-	{
-		free(SEARCH_PATHS[i]);
-	}
-
-	free(SEARCH_PATHS);
-	SEARCH_PATHS_LEN = n;
-	paths = calloc(n, sizeof(char *));
-
-	for(size_t i = 0; i < n; i++)
-	{
-		SEARCH_PATHS[i] = strdup(paths[i]);
-	}
-}
-
-char *absolute_path(char *name)
-{
-	char *path = NULL;
-	int path_len = 0;
-
-	if(name == NULL || name[0] == '\0')
-	{
-		return NULL;
-	}
-
-	if(name[0] == '/')
-	{
-		return name;
-	}
-	
-	for(size_t i = 0; i < SEARCH_PATHS_LEN; i++)
-	{
-		path_len = strlen(SEARCH_PATHS[i]) + strlen(name) + 1;
-		path = calloc(path_len + 1, sizeof(char));
-		snprintf(path, path_len + 1, "%s/%s", SEARCH_PATHS[i], name);
-		
-		if(access(path, X_OK) == 0)
-		{
-			return strdup(path);
-		}
-
-		free(path);
-	}
-	
 	return NULL;
 }
 
-void execute_command(char **args, size_t n)
+void run_builtin(char **args, size_t args_len)
 {
-	char *abs_path = NULL;
-
-	if(strcmp(args[0], "exit") && n == 1)
+	if(strcmp(args[0], "exit") == 0 && args_len == 1)
 	{
-		shell_exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS);
 	}
-	else if(strcmp(args[0], "chdir") && n == 2)
+	else if(strcmp(args[0], "cd") == 0 && args_len == 2)
 	{
-		shell_chdir(args[1]);
-	}
-	else if(strcmp(args[0], "path"))
-	{
-		shell_path(args + 1, n - 1);
-	}
-	else if((abs_path = absolute_path(args[0])) != NULL)
-	{
-		if(fork() == 0)
+		chdir(args[1]);
+		if(errno != 0)
 		{
-			execv(abs_path, args);
+			fprintf(stderr, ERROR);
+			errno = 0;
 		}
+	}
+	else if(strcmp(args[0], "path") == 0)
+	{
+		update_search_paths(args + 1, args_len - 1);
 	}
 	else
 	{
@@ -102,18 +49,110 @@ void execute_command(char **args, size_t n)
 	}
 }
 
+void run_program(char *name, char **args, size_t args_len, FILE *redirect)
+{
+}
+
+void run_cmd(char **args, size_t args_len, FILE *redirect)
+{
+	char *exec_path = absolute_path(args[0]);
+
+	if(exec_path == NULL)
+	{
+		run_builtin(args, args_len);
+	}
+	else
+	{
+		run_program(exec_path, args, args_len, redirect);
+	}
+}
+
+void exec_cmd(char *cmd, size_t cmd_len)
+{
+	/* The maximum possible number of arguments occurs when one
+	 * character arguments alternate with space. Example: "a a a"
+	 * has 5 characters and three arguments. 5 / 2 + 1 = 3
+	 */
+	char *args[cmd_len / 2 + 1];
+	size_t args_len = 0;
+	char *arg = strsep(&cmd, ARG_SEP);
+	bool open_redirect = false;
+	FILE *redirect_file = NULL;
+
+	while(arg != NULL)
+	{
+		/* If there is arbitrary space, argument may be
+		 * empty. Do nothing.
+		 */
+		if(*arg == '\0')
+		{
+		}
+		else if(open_redirect)
+		{
+			open_redirect = false;
+			if((redirect_file = fopen(arg, "w")) == NULL)
+			{
+				fprintf(stderr, ERROR);
+				exit(EXIT_FAILURE);
+			}
+		}
+		/* Redirection can possibly occur with or without
+		 * space separating the operator. An argument with a
+		 * redirection operator needs special handling.
+		 */
+		else if(strchr(arg, '>') == NULL)
+		{
+			/* No redirection. Easy. */
+			args[args_len] = arg;
+			args_len++;
+		}
+		else
+		{
+			open_redirect = true;
+			arg = strsep(&cmd, ">");
+
+			/* Possibly an argument before the redirect operator. */
+			if(*arg != '\0')
+			{
+				args[args_len] = arg;
+				args_len++;
+			}
+		}
+
+		arg = strsep(&cmd, ARG_SEP);
+	}
+
+	run_cmd(args, args_len, redirect_file);
+}
+
+/* For the scope given, take advantage of the fact that the only time
+ * we ever need to wait for children commands when we have finished
+ * processing the line.
+ */
+void exec_line(char *line, size_t len)
+{
+	char *cmd = strsep(&line, CMD_SEP);
+
+	while(cmd != NULL)
+	{
+		/* Since line is updated to point after the null byte,
+		 * length of command string is given by line - cmd
+		 * - 1.
+		 */
+		exec_cmd(cmd, line - cmd - 1);
+		cmd = strsep(&line, "&\n");
+	}
+
+	/* Now wait for all children to finish. */
+	while(wait(NULL) != ECHILD);
+}
+
 int main(int argc, char **argv)
 {
-	char *line = NULL;
-	char *line_pos = NULL;
-	char *token = NULL;
-	size_t n = 0;
-	ssize_t line_len = 0;
-
 	FILE *input = stdin;
-
-	char **args = NULL;
-	size_t args_len = 0;
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t line_len;
 
 	if(argc > 2)
 	{
@@ -121,51 +160,15 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if(argc == 2)
+	if(argc == 2 && (input = fopen(argv[1], "r")) == NULL)
 	{
-		if((input = fopen(argv[1], "r")) == NULL)
-		{
-			fprintf(stderr, ERROR);
-			return EXIT_FAILURE;
-		}
-	}
-	else
-	{
-		fprintf(stdout, PROMPT);
+		fprintf(stderr, ERROR);
+		return EXIT_FAILURE;
 	}
 
 	while((line_len = getline(&line, &n, input)) != -1)
 	{
-		line_pos = line;
-		token = strsep(&line_pos, " \f\n\r\t\v");
-		/* Maximum possible length of argument list occurs
-		 * with one character arguments every other
-		 * position. Example: "a a a" has three arguments and
-		 * five characters. 5 / 2 + 1 = 3. */
-		args = calloc(line_len / 2 + 1, sizeof(char *));
-		while(token != NULL)
-		{
-			if(*line_pos == '\0' || *token == ';' || *token == '&')
-			{
-				execute_command(args, args_len);
-				while(*token != '&' && wait(NULL) != ECHILD);
-				args_len = 0;
-			}
-			else
-			{
-				args[args_len] = token;
-				args_len++;
-			}
-
-			token = strsep(&line_pos, " \f\n\r\t\v");
-		}
-
-		free(args);
-
-		if(input == stdin)
-		{
-			fprintf(stdout, PROMPT);
-		}
+		exec_line(line, line_len);
 	}
 
 	free(line);
