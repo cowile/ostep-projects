907ed579d393b65d12b28d79cca003997ca0814b
Colton Lewis <colton.w.lewis@protonmail.com>
2020-10-10 03:07:00 -0500

    Actually implement mprotect.

--- xv6-null-pages/src/mprotect.c
+++ xv6-null-pages/src/mprotect.c
@@ -5,18 +5,39 @@
 #include "defs.h"
 #include "x86.h"
 
+// mset
+// Starting at addr, set len pages with the given permissions.
+int mset(void *addr, int len, int set, int flag)
+{
+  struct proc *cuproc = myproc();
+  pte_t *pgdir = curproc()->pgdir;
+  pte_t *pgtab;
+
+  for(int i = 0; i < len; i++)
+  {
+    if((pgtab = walkpgdir(pgdir, addr + (i * PGSIZE), 0)) == 0)
+      return -1;
+    if(set)
+      *pgtab = PTE_ADDR(*pgtab) | flag;
+    else
+      *pgtab = PTE_ADDR(*pgtab) & ~flag;
+  }
+  return 0;
+}
+
 // mprotect
-// Starting at addr, mark len pages read only.
+// Set pages read-only.
+
 int mprotect(void *addr, int len)
 {
-  return 0;
+  return mset(addr, len, 1, PTE_W);
 }
 
 // munprotect
 // Reverse mprotect.
 int munprotect(void *addr, int len)
 {
-  return 0;
+  return mset(addr, len, 0, PTE_W);
 }
 
 int sys_mprotect(void)
@@ -24,8 +45,10 @@ int sys_mprotect(void)
   char *addr;
   int len;
 
+  // Must hae positive len.
   if(argint(1, &len) < 0 || len <= 0)
     return -1;
+  // len * PGSIZE is the size of memory we protect. Must be page aligned.
   if(argptr(0, &addr, len * PGSIZE) < 0 || (uint)addr % PGSIZE != 0)
     return -1;
   if(mprotect(addr, len))
